11a3fbdad8918115dbd0f53c130b2600968abaff
owenpf <ouwenpf@gmail.com>
2019-09-12 14:15:08 +0800

    20190912

---
+++ other/new 3.bash
@@ -0,0 +1,107 @@
+yum install -y php71w php71w-bcmath php71w-cli php71w-common php71w-dba php71w-devel php71w-embedded php71w-enchant php71w-fpm php71w-gd php71w-imap php71w-interbase php71w-intl php71w-ldap php71w-mbstring php71w-mcrypt php71w-mysql php71w-mysqlnd php71w-odbc php71w-opcache php71w-pdo php71w-pdo_dblib php71w-pear php71w-pecl-apcu php71w-pecl-imagick php71w-pecl-xdebug php71w-pgsql php71w-phpdbg php71w-process php71w-pspell php71w-recode php71w-snmp php71w-soap php71w-tidy php71w-xml php71w-xmlrpc
+
+
+rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+
+yum install -y php71w php71w-bcmath php71w-cli php71w-common php71w-dba php71w-devel php71w-embedded php71w-enchant php71w-fpm php71w-gd php71w-imap php71w-intl php71w-ldap php71w-mbstring php71w-mysqlnd php71w-odbc php71w-opcache php71w-pdo php71w-pear php71w-pecl-apcu php71w-pecl-imagick php71w-pecl-xdebug php71w-pgsql php71w-phpdbg php71w-process php71w-pspell php71w-recode php71w-snmp php71w-soap php71w-tidy php71w-xml php71w-xmlrpc
+
+
+yum install -y libpng-devel,libmcrypt,libtool-ltdl,libjpeg-turbo,libjpeg-turbo-devel,freetype 
+
+
+
+change master to \
+master_host='127.0.0.1', \
+master_port=3306, \
+master_user='repl', \
+master_password='123456',\
+MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=31092309;
+
+mysql-bin.000002 | 26577460
+
+master_auto_position=1;
+
+create table pressure.tb1 (
+	id int not null auto_increment,
+	name varchar(10) not null,
+	primary key (id)
+);
+
+31092309
+
+阿里云：
+xinxinya356	SPQsTi9QWwc7vgxaFgKK
+
+腾讯云：
+haulcony@gmail.com	Gameads@2019
+hben7573@gmail.com	Gameads@2019
+
+香港游戏服
+wget -c  'http://rdsbak-hz-v3.oss-cn-hangzhou.aliyuncs.com/custins4211263/hins2915505_data_20190705143130.tar?OSSAccessKeyId=LTAIyKzxtSYNknVO&Expires=1562484389&Signature=feG425rhl4DS0hgFwLWHBSIqd2c%3D' -O tbmj.sql.tar.gz 
+
+
+恩施代理：esmj.renren6.com 114.55.179.39 10.26.92.204 Gameads@2019  mysql：Diandoojaajia201i6
+恩施麻将api：116.62.30.218  10.27.235.85 Dianjia2017!@#123 
+支付：pay.renren6.com 		 116.62.38.206 10.28.46.15 	http://pay.renren6.com/adminer.php  mysql：Dianjia2016!@#
+	  test.pay.renren6.com	 120.26.96.30  10.51.25.87 Dianjia2017!@#123
+
+ssh  daq2.djgame.net 114.55.105.232 10.26.235.48
+ssh 13410617275@39.108.104.160 -p60022
+ssh root@mm.renren6.com
++86 13316043531   或者 @Swenhr
+
+
+麻将订单校验服务 118.178.106.222 10.26.96.31 Dianjia2017!@#123     mjjy, R&74Qq4vR  "gdy_mjjy", "R&74Qq4vE"
+
+阿里牛牛账号dianjia2017    www.kk8.com
+			麻将：djwl2016       Doojaa@2020
+			3A：gameads2017  Gameads@2019  
+			http://walle.game3a.com/login
+			owner@walle-web.io 
+			Gameads@2020
+牛牛api 119.23.38.19  Dianjia2017!@#123
+
+3A: ssh yw001@148.66.61.234 -p2222    密码 Gameads@yw001 
+es服务器：ssh -p2222 yw001@47.111.153.70    密码 Enshi@@2020
+
+
+
+47.112.140.132  47.105.195.196 Dianjia2017!@#123  mysql -uroot -p    tongbei!@#123
+47.111.2.151  Gameads@2020   数据库备份  47.111.2.151:8080
+107.167.5.60  Gameads@2020   711游戏备份 /data/711data
+
+
+
+
+http://daq2.djgame.net
+@l03e1t3
+
+
+
+
+z9mLwUv6Pr9RBpMh
+
+
+
+
+game-master.mysql.rds.aliyuncs.com  HJdianjiaWEB2016  webhj
+.zjhmysql  user_hongbao_record
+
+
+http://118.178.106.222:52553/adminer.php
+
+恩施麻将游戏： mysql://renren6:Dianjia2016@majianglog001.mysql.rds.aliyuncs.com:3306/esmj#utf8
+r-bp1bf531b1782914  Doojaa2016
+
+  
+  echo  'Enshimj!@#123'|passwd --stdin root
+
+	
+	
+https://www.netflav.com/all?actress=%E4%B8%89%E6%B5%A6%E6%81%B5%E7%90%86%E5%AD%90
+
+http://www.jingdiand.xyz:8080/ 临时下载
+http://www.jingdiand.xyz:52553/adminer.php
+
+
+
