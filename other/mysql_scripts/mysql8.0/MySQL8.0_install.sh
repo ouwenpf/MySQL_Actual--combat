cf1bfc53714c4d7e027db40d04bf29188a4b335a
ouwenpf <ouwenpf@gmail.com>
2023-12-13 00:11:00 +0800

    20231213

--- other/mysql_scripts/mysql8.0/MySQL8.0_install.sh
+++ other/mysql_scripts/mysql8.0/MySQL8.0_install.sh
@@ -1,111 +1,163 @@
-#########################################################################
-# File Name: MySQL_Install.sh
-# Author: Owen
-# mail: 
-# Created Time: 2019-11-07 wed 
-# package：mysql-5.7.24-linux-glibc2.12-x86_64
-# Discription: mkdir /application  tar
-#########################################################################
+#!/bin/bash
 
-#!/bin/sh
-#
-base_dir="/application/mysql-8.0.17"
-ip_str=`ip a|grep -A 3 'mtu 1500'|awk  -F '[ /]+' 'NR==3{print $3}'|awk -F '.' '{print $NF}'`
-#server_id=${ip_str: -3}
-server_id=$1${ip_str}
+#klunstron_user=${1:-kunlun}
+#klunstron_basedir=${2:-/home/kunlun/klustron}
+klunstron_user=$1
+klunstron_basedir=$2
 
-if [ $# -eq 0 ];then
-	 echo "Usage: $0  {3306|3307|...}"
-	 exit 
-fi
+if [ `ping 8.8.8.8 -c 3 | grep "min/avg/max" -c` = '1' ]; then
 
-if [ ! -d ${base_dir} ];then
-	 echo "First create the application directory, MySQL package storage location"
-	 exit
-fi
+	if [[  -f /etc/rc.d/init.d/functions ]];then
+		. /etc/rc.d/init.d/functions
 
-if [ ! -f ${base_dir}/my.cnf ];then
-	echo 'file my.cnf not exist'
-	exit
-fi
-
-#
-env(){ 
-if [ `ping 8.8.8.8 -c 5 | grep "min/avg/max" -c` = '1' ]; then
-	yum install -y wget 
-        wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo  
-        wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo  
-        yum install -y numactl-libs libaio man bash-completion man-pages-zh-CN.noarch iptables-services lrzsz tree screen telnet dosunix nmap htop openssl openssh openssl-devel bind-utils iotop nc dstat yum-utils*  psacct
+	else 
+		yum install -y initscripts  &>/dev/null   &\
+		. /etc/rc.d/init.d/functions
+		# success failure
+	
+	fi 
 else
-    	echo "The network connection failed and the installation was terminated!"
-    	exit
-fi
+	echo "No network"  
+	exit
+
+fi 
+
+function Msg(){
+    if $1; then
+        action "$1"  /bin/true
+    else
+		action "$1"  /bin/false   
+    fi
 }
 
-# 
-if ! id mysql &> /dev/null ;then
-	useradd -r -M -s /sbin/nologin mysql
-fi
 
-# 
-if [ ! -d /data/mysql/mysql$1 ];then
-	mkdir -p /data/mysql/mysql$1/{data,logs,tmp}
-	chown -R mysql.mysql /data/mysql/mysql$1
+
+
+if [ $# -ne 2 ];then
+	action "Usage:  please input klunstron_user and klunstron_basedir" /bin/false
+	exit 
 fi
 
 
-if [ ! -f /data/mysql/mysql$1/my.cnf ];then
 
-   	cp  ${base_dir}/my.cnf /data/mysql/mysql$1 &&\
-	sed -ri  's/3306/'$1'/g'  /data/mysql/mysql$1/my.cnf
-	sed  -ri  '16s/'$1'/'${server_id}'/g'  /data/mysql/mysql$1/my.cnf	
+
+
+
+
+if ! id $klunstron_user &>/dev/null;then 
+	#groupadd -g 1007 $klunstron_user 
+    #useradd  -u 1007 -g 1007 $klunstron_user
+	useradd  $klunstron_user  &>/dev/null   &&\
+	echo 'kunlun#'|passwd  --stdin $klunstron_user &>/dev/null &&\
+    sed -ri '/Allow root to run any commands anywhere/a '${klunstron_user}'  ALL=(ALL)  NOPASSWD: ALL'  /etc/sudoers
+	if [[ $? == 0 ]];then
+		action "$1 User created successfully"  /bin/true
+	else
+		action "useradd: user $1 already exists"  /bin/false
+	fi 
 fi
+
+
+
+
+yum remove  -y  postfix mariadb-libs  &>/dev/null
+if [[ $? == 0 ]];then
+	action "postfix mariadb-libs Uninstallation successful"  /bin/true
+else
+	action "postfix mariadb-libsr Uninstallation failed"  /bin/false
+fi 
+
+
+
+
+yum install -y python git wget yum-utils sysvinit-tools libaio libaio-devel expect chrony python3-3.6.8-18.el7.x86_64 &>/dev/null
+if [[ $? == 0 ]];then
+	action "Basic package installation successful"  /bin/true
+else
+	action "Basic package installation  failed"  /bin/false
+fi 
+
+
+
+#for i in python git wget yum-utils sysvinit-tools libaio libaio-devel expect chrony python3-3.6.8-18.el7.x86_64
+#do
+#	if ! rpm -qa|grep  -w $i &>/dev/null;then
+#	yum install -y $i  &>/dev/null
+#	fi 
+#done
+
+
+if [ `timedatectl|grep -c 'Asia/Shanghai'` -eq 0 ];then
+	timedatectl set-timezone Asia/Shanghai
+	if [[ $? == 0 ]];then
+		action "Time zone configuration successful"  /bin/true
+	else
+		action "Time zone configuration  failed"  /bin/false
+	fi 
+else 
+	action  "The time zone has been successfully configured" /bin/true
+	
+fi
+
+if [[ -f  /etc/selinux/config ]];then
+	setenforce 0 &&\
+	sed -ri  's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+	if [[ $? == 0 ]];then
+		action "SELINUX configuration successful"  /bin/true
+	else
+		action "SELINUX configuration  failed"  /bin/false
+	fi 
+else 
+	action "SELINUX No configuration because not installed"  /bin/true
+fi
+
+if [[ -f  /etc/security/limits.conf ]];then
+cat >> /etc/security/limits.conf << EOF
+*                soft    core          unlimited
+*                hard    core          unlimited
+*                soft    nproc         1000000
+*                hard    nproc         1000000
+*                soft    nofile        100000
+*                hard    nofile        100000
+*                soft    memlock       32000
+*                hard    memlock       32000
+*                soft    msgqueue      8192000
+*                hard    msgqueue      8192000
+EOF
+	if [[ $? == 0 ]];then
+		action "limits configuration successful"  /bin/true
+	else
+		action "limits configuration  failed"  /bin/false
+	fi 
+
+fi
+
+
  
-
-if [ ! -d /usr/local/mysql ];then
-   	 ln -s ${base_dir}    /usr/local/mysql
-fi
-
-
-
-
-#PATH_file
-if [ ! -f /etc/profile.d/mysql.sh ];then
-	echo 'export PATH=/usr/local/mysql/bin:$PATH' >> /etc/profile.d/mysql.sh
-	source /etc/profile.d/mysql.sh
-fi
-
-#lib64_file
-if [ ! -f /etc/ld.so.conf.d/mysql.conf ];then
-	echo '/usr/local/mysql/lib' > /etc/ld.so.conf.d/mysql.conf
-fi
-
-#include_file
-if [ ! -d /usr/include/mysql ];then
-	ln -s /usr/local/mysql/include/ /usr/include/mysql 
-fi
-
-#man_file
-if [ ! -f /etc/man_db.conf ];then
-	sed -ri '22a \MANDATORY_MANPATH                       /usr/local/mysql/man'  /etc/man_db.conf
-
-fi
-# 
-/usr/local/mysql/bin/mysqld --defaults-file=/data/mysql/mysql$1/my.cnf  --initialize-insecure 
-sleep 5
-if [ $? -eq 0 ];then
-	echo "MySQL initialization succeeded"
+systemctl stop firewalld &>/dev/null && systemctl disable firewalld   &>/dev/null
+if [[ $? == 0 ]];then
+	action "The firewall has been closed successful"  /bin/true
 else
-	echo "MySQL initialization failed"
-	exit
-fi
+	action "The firewall has been closed  failed"  /bin/true
+fi 
 
-# 
-/usr/local/mysql/bin/mysqld --defaults-file=/data/mysql/mysql$1/my.cnf & &> /dev/null
-sleep 5
+systemctl enable chronyd && systemctl start chronyd &>/dev/null
 
-if /usr/local/mysql/bin/mysql -S /tmp/mysql3306.sock  -e 'select @@server_id;' &> /dev/null;then
-	echo "MySQL started successfully"
+if [[ $? == 0 ]];then
+	action "Time synchronization server successful"  /bin/true
 else
-	echo "MySQL startup failed"
+	action "Time synchronization server  failed"  /bin/false
+fi 
+
+
+
+if [[ ! -d  $klunstron_basedir ]];then
+
+	mkdir -p $klunstron_basedir  && chown -R $klunstron_user:$klunstron_user $klunstron_basedir  &>/dev/null
+	if [[ $? == 0 ]];then
+		action "Database directory creation successful"  /bin/true
+	else
+		action "Database directory creation  failed"  /bin/false
+	fi 
+
 fi
